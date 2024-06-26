7df3cee904fd35c1f062b7585010c1dd9957e355
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-06-26 16:58:56 -0300

    feat: payment-api: update streamming config implement consumer process and add suplier

--- payment-api/src/main/java/me/dio/hiokdev/e_commerce_payment_api/config/StreamingConfig.java
+++ payment-api/src/main/java/me/dio/hiokdev/e_commerce_payment_api/config/StreamingConfig.java
@@ -3,12 +3,18 @@ package me.dio.hiokdev.e_commerce_payment_api.config;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import lombok.extern.slf4j.Slf4j;
 import me.dio.hiokdev.e_commerce_checkout_api.checkout.event.CheckoutCreatedEvent;
+import me.dio.hiokdev.e_commerce_payment_api.payment.event.PaymentPaidEvent;
+import me.dio.hiokdev.e_commerce_payment_api.service.PaymentService;
+import me.dio.hiokdev.e_commerce_payment_api.streaming.PaymentPaidProducer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Slf4j
 @Configuration
@@ -20,9 +26,31 @@ public class StreamingConfig {
     }
 
     @Bean
-    public Consumer<KStream<CheckoutCreatedEvent, CheckoutCreatedEvent>> checkoutCreatedEventConsumer() {
-        return stream -> stream.peek((key, value) -> log
-                .info(String.format("#### -> Consuming message: checkoutCode: -> %s", value.getCheckoutCode())));
+    public Consumer<KStream<CheckoutCreatedEvent, CheckoutCreatedEvent>> checkoutCreatedEventConsumer(
+            PaymentService paymentService,
+            PaymentPaidProducer paymentPaidProducer
+    ) {
+        return stream -> stream.peek((key, value) -> {
+            log.info(String.format("#### -> Consuming message: checkoutCode: -> %s", value.getCheckoutCode()));
+            final var payment = paymentService.pay(value).orElseThrow();
+            final var paymentPaidEvent = PaymentPaidEvent.newBuilder()
+                    .setPaymentCode(payment.getCode())
+                    .setCheckoutCode(payment.getCheckoutCode())
+                    .build();
+            paymentPaidProducer.send(paymentPaidEvent);
+        });
+    }
+
+    @Bean
+    public BlockingQueue<PaymentPaidEvent> paymentPaidEventBlockingQueue() {
+        return new LinkedBlockingQueue<>();
+    }
+
+    @Bean
+    public Supplier<PaymentPaidEvent> paymentPaidEventSupplier(
+            BlockingQueue<PaymentPaidEvent> paymentPaidEventBlockingQueue
+    ) {
+        return paymentPaidEventBlockingQueue::poll;
     }
 
 }
