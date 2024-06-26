dd11fc24b772c91c3ec401d9c64ecf8c39787881
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-06-26 00:31:08 -0300

    feat: create payment-api java spring project and add dependencies

---
+++ payment-api/src/main/java/me/dio/hiokdev/e_commerce_payment_api/ECommercePaymentApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.hiokdev.e_commerce_payment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercePaymentApiApplication.class, args);
+	}
+
+}
---
+++ payment-api/src/test/java/me/dio/hiokdev/e_commerce_payment_api/ECommercePaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.hiokdev.e_commerce_payment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercePaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
