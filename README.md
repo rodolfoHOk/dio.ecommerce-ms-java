405bb79d38cb07b56c915457692a9f4493b98e54
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-06-26 17:56:49 -0300

    feat: checkout-api: create config payment paid event consumer

--- README.md
+++ README.md
@@ -6,10 +6,17 @@ Neste projeto prático iremos desenvolver uma solução de e-commerce com a arqu
 
 - Java 21
 - Spring Framework / Spring Boot
-- Spring Cloud / Stream and Distributed Tracing
+- Spring Cloud Stream with Distributed Tracing
 - Apache Kafka
 - Apache Avro
 - Schema Registry
+- PostgreSQL
+
+## Atualizações de versões:
+
+- Spring Boot 3.3
+- Spring Cloud Stream 4.1
+- ConfluentInc Kafka and Schema Registry 7.6.1
 
 ## Introdução ao projeto e suas tecnologias
 
@@ -24,5 +31,3 @@ Neste projeto prático iremos desenvolver uma solução de e-commerce com a arqu
 [Spring Cloud Stream Kafka Binder Reference Guide](https://docs.spring.io/spring-cloud-stream/docs/current/reference/html/spring-cloud-stream-binder-kafka.html)
 
 [Spring initializr: e-commerce-payment-api](https://start.spring.io/#!type=gradle-project&language=java&platformVersion=3.3.1&packaging=jar&jvmVersion=21&groupId=me.dio.hiokdev&artifactId=e-commerce-payment-api&name=e-commerce-payment-api&description=E-commerce%20Checkout%20API%20Microservice&packageName=me.dio.hiokdev.e-commerce-payment-api&dependencies=web,cloud-stream,distributed-tracing,kafka-streams,lombok,data-jpa,postgresql,validation,kafka)
-
-##### [lab](https://web.dio.me/lab/criando-uma-solucao-de-e-commerce-com-microsservicos-em-java/learning/1334e836-1886-4c47-b44b-944d9fb544e2?back=/play) parte 12
--- checkout-api/src/main/java/me/dio/hiokdev/e_commerce_checkout_api/config/StreamingConfig.java
+++ checkout-api/src/main/java/me/dio/hiokdev/e_commerce_checkout_api/config/StreamingConfig.java
@@ -1,22 +1,22 @@
 package me.dio.hiokdev.e_commerce_checkout_api.config;
 
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import lombok.extern.slf4j.Slf4j;
 import me.dio.hiokdev.e_commerce_checkout_api.checkout.event.CheckoutCreatedEvent;
+import me.dio.hiokdev.e_commerce_payment_api.payment.event.PaymentPaidEvent;
 import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+@Slf4j
 @Configuration
 public class StreamingConfig {
-
-    @Bean
-    public Serde<CheckoutCreatedEvent> avroInSerde() {
-        return new SpecificAvroSerde<>();
-    }
 
     @Bean
     public BlockingQueue<CheckoutCreatedEvent> checkoutCreatedEventBlockingQueue() {
@@ -28,6 +28,18 @@ public class StreamingConfig {
             BlockingQueue<CheckoutCreatedEvent> checkoutCreatedEventBlockingQueue
     ) {
         return checkoutCreatedEventBlockingQueue::poll;
+    }
+
+    @Bean
+    public Serde<PaymentPaidEvent> avroInSerde() {
+        return new SpecificAvroSerde<>();
+    }
+
+    @Bean
+    public Consumer<KStream<PaymentPaidEvent, PaymentPaidEvent>> paymentPaidEventConsumer() {
+        return stream -> stream.peek((key, value) -> {
+            log.info(String.format("#### -> Consuming message: paymentCode: -> %s", value.getPaymentCode()));
+        });
     }
 
 }
