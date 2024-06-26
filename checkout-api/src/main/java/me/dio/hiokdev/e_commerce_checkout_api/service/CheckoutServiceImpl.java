ad16d07ec00e70752f8f3edb4e71ef7d895808cc
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-06-26 00:16:31 -0300

    feat: checkout-api: use checkout created producer in checkout service impl

--- checkout-api/src/main/java/me/dio/hiokdev/e_commerce_checkout_api/service/CheckoutServiceImpl.java
+++ checkout-api/src/main/java/me/dio/hiokdev/e_commerce_checkout_api/service/CheckoutServiceImpl.java
@@ -1,9 +1,11 @@
 package me.dio.hiokdev.e_commerce_checkout_api.service;
 
 import lombok.RequiredArgsConstructor;
+import me.dio.hiokdev.e_commerce_checkout_api.checkout.event.CheckoutCreatedEvent;
 import me.dio.hiokdev.e_commerce_checkout_api.entity.CheckoutEntity;
 import me.dio.hiokdev.e_commerce_checkout_api.repository.CheckoutRepository;
 import me.dio.hiokdev.e_commerce_checkout_api.resource.checkout.CheckoutRequest;
+import me.dio.hiokdev.e_commerce_checkout_api.streaming.CheckoutCreatedProducer;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,6 +16,7 @@ import java.util.UUID;
 public class CheckoutServiceImpl implements CheckoutService {
 
     private final CheckoutRepository checkoutRepository;
+    private final CheckoutCreatedProducer checkoutCreatedProducer;
 
     @Override
     public Optional<CheckoutEntity> create(CheckoutRequest checkoutRequest) {
@@ -21,7 +24,14 @@ public class CheckoutServiceImpl implements CheckoutService {
                 .code(UUID.randomUUID().toString())
                 .status(CheckoutEntity.Status.CREATED)
                 .build();
-        var savedEntity = checkoutRepository.save(checkoutEntity);
+        final var savedEntity = checkoutRepository.save(checkoutEntity);
+
+        final var checkoutCreatedEvent = CheckoutCreatedEvent.newBuilder()
+                .setCheckoutCode(savedEntity.getCode())
+                .setStatus(savedEntity.getStatus().name())
+                .build();
+        checkoutCreatedProducer.send(checkoutCreatedEvent);
+
         return Optional.of(savedEntity);
     }
 
