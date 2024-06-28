07c8239419e51e2403736f160e692c61bf1d69fb
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-06-27 23:28:15 -0300

    tests: checkout-api: remove intermediary Inheritance

--- checkout-api/src/test/java/me/dio/hiokdev/e_commerce_checkout_api/steps/CheckoutDefinitions.java
+++ checkout-api/src/test/java/me/dio/hiokdev/e_commerce_checkout_api/steps/CheckoutDefinitions.java
@@ -1,15 +1,12 @@
 package me.dio.hiokdev.e_commerce_checkout_api.steps;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
 import me.dio.hiokdev.e_commerce_checkout_api.resource.checkout.CheckoutRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
@@ -19,14 +16,8 @@ import org.springframework.web.client.HttpStatusCodeException;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
-public class CheckoutDefinitions {
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
-    private ObjectMapper objectMapper;
+@CucumberContextConfiguration
+public class CheckoutDefinitions extends CucumberSpringIntegrationTest {
 
     private String responseBody;
     private HttpHeaders responseHeaders;
--- checkout-api/src/test/java/me/dio/hiokdev/e_commerce_checkout_api/steps/CucumberSpringIntegrationTest.java
+++ checkout-api/src/test/java/me/dio/hiokdev/e_commerce_checkout_api/steps/CucumberSpringIntegrationTest.java
@@ -1,6 +1,10 @@
-package me.dio.hiokdev.e_commerce_checkout_api;
+package me.dio.hiokdev.e_commerce_checkout_api.steps;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.dio.hiokdev.e_commerce_checkout_api.ECommerceCheckoutApiApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -10,7 +14,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {ECommerceCheckoutApiApplication.class})
-public abstract class SpringIntegrationTest {
+public abstract class CucumberSpringIntegrationTest {
 
     @Container
     @ServiceConnection
@@ -18,5 +22,16 @@ public abstract class SpringIntegrationTest {
             .withDatabaseName("checkout")
             .withUsername("admin")
             .withPassword("admin");
+
+    @Autowired
+    protected TestRestTemplate restTemplate;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+//    @Test
+//    void testContainer() {
+//        assertThat(postgres.isRunning()).isTrue();
+//    }
 
 }
